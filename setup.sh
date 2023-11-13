dd5a9f1d25fb0eaa22e25898b1b373b44558b03d
y-kamiya <y.kamiya0@gmail.com>
2023-11-13 19:34:26 +0900

    setup for specific env

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ ln -i -s $SCRIPT_DIR/.xvimrc $HOME/.xvimrc
 ln -i -s $SCRIPT_DIR/.zshrc $HOME/.zshrc
 ln -i -s $SCRIPT_DIR/.zprofile $HOME/.zprofile
 ln -i -s $SCRIPT_DIR/.ideavimrc $HOME/.ideavimrc
+ln -i -s $SCRIPT_DIR/.zshrc_env $HOME/.zshrc_env
+ln -i -s $SCRIPT_DIR/.vimrc.local $HOME/.vimrc.local
+ln -i -s $SCRIPT_DIR/.gitconfig.local $HOME/.gitconfig.local
 
 mkdir -p $HOME/.config
 ln -i -s $SCRIPT_DIR/vim $HOME/.config/nvim
@@ -20,5 +23,13 @@ pushd $SCRIPT_DIR/vim
 ./create_python_env.sh
 popd
 
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+if [ ! -d $HOME/.tmux ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+if [ ! -d $HOME/.asdf ]; then
+    git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+fi
+
+if [ -f $SCRIPT_DIR/setup_local.sh ]; then
+    ./setup_local.sh
+fi
